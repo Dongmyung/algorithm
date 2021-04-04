96f2f5d3bdcc86fc1025ba36bba9e25e01696de0
Dongmyung Ahn <crazyjumong@gmail.com>
2021-04-04 22:31:23 +0900

    Update Lesson04_MaxCounters.kt

--- Lesson04_MaxCounters.kt
+++ Lesson04_MaxCounters.kt
@@ -1,16 +1,19 @@
 fun solution(N: Int, A: IntArray): IntArray {
     var counter = IntArray(N)
     var max = 0
+    var checkedMax = 0
     for (i in A.indices) {
         if (A[i] <= N) {
+            if (counter[A[i]-1] < checkedMax) counter[A[i]-1] = checkedMax
             counter[A[i]-1]++
             if (max < counter[A[i]-1]) max = counter[A[i]-1]
         } else {
             // Max Counter
-            for (idx in counter.indices) {
-                counter[idx] = max
-            }
+            checkedMax = max
         }
+    }
+    for (i in counter.indices) {
+        if (counter[i] < checkedMax) counter[i] = checkedMax
     }
     return counter
 }
