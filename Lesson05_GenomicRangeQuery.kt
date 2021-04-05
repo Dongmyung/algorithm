4e1b7c504c51e8c46244ed86b51eb8f1538f8550
Dongmyung Ahn <crazyjumong@gmail.com>
2021-04-05 18:09:39 +0900

    Update Lesson05_GenomicRangeQuery.kt
    Using Counters. Understand after google search.

--- Lesson05_GenomicRangeQuery.kt
+++ Lesson05_GenomicRangeQuery.kt
@@ -1,27 +1,47 @@
 fun solution(S: String, P: IntArray, Q: IntArray): IntArray {
     var ret = IntArray(P.size)
+    var counterA = IntArray(S.length+1)
+    var counterC = IntArray(S.length+1)
+    var counterG = IntArray(S.length+1)
+    for (i in S.indices) {
+        counterA[i+1] = counterA[i]
+        counterC[i+1] = counterC[i]
+        counterG[i+1] = counterG[i]
+
+        val c = S[i]
+        when(c) {
+            'A' -> counterA[i+1]++
+            'C' -> counterC[i+1]++
+            'G' -> counterG[i+1]++
+            else -> {}
+        }
+    }
+
     for (i in 0..P.lastIndex) {
-        val subStr = S.slice(P[i]..Q[i])
-        ret[i] = getMinImpactFactor(subStr)
+        val startIdx = P[i]
+        val endIdx = Q[i]
+
+        if (startIdx == endIdx) {
+            val c = S[startIdx]
+            ret[i] = when (c) {
+                'A' -> 1
+                'C' -> 2
+                'G' -> 3
+                'T' -> 4
+                else -> -1
+            }
+        } else {
+            if (counterA[startIdx] != counterA[endIdx+1]) {
+                ret[i] = 1
+            } else if (counterC[startIdx] != counterC[endIdx+1]) {
+                ret[i] = 2
+            } else if (counterG[startIdx] != counterG[endIdx+1]) {
+                ret[i] = 3
+            } else {
+                ret[i] = 4
+            } 
+        }
     }
+
     return ret
-}
-
-fun getMinImpactFactor(S: String): Int {
-    var min = Int.MAX_VALUE
-    for (s in S) {
-        val impactFactor = getImpactFactor(s)
-        if (impactFactor < min) min = impactFactor
-    }
-    return min
-}
-
-fun getImpactFactor(c: Char): Int {
-    return when(c) {
-        'A' -> 1
-        'C' -> 2
-        'G' -> 3
-        'T' -> 4
-        else -> -1
-    }
 }
