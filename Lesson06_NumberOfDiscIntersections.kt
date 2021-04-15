1d682db312f5bea4ec0106408f1bc7cb983a3791
Dongmyung Ahn <crazyjumong@gmail.com>
2021-04-15 09:50:29 +0900

    Found better performance logic from internet

--- Lesson06_NumberOfDiscIntersections.kt
+++ Lesson06_NumberOfDiscIntersections.kt
@@ -1,23 +1,24 @@
 fun solution(A: IntArray): Int {
     val N = A.size
-    val list = ArrayList<Pair<Long,Long>>()
+    var cnt = 0
+    var upper = LongArray(N)
+    var lower = LongArray(N)
     for (i in 0 until N) {
-        list.add(Pair(i-A[i].toLong(), i+A[i].toLong()))
+        upper[i] = (i+A[i].toLong())
+        lower[i] = (i-A[i].toLong())
     }
-    var sortedList = list.sortedWith(compareBy({it.first}))
-    var count = 0
-    for (i in 0 until N-1) {
-        for (j in i+1 until N) {
-            if (sortedList[j].first > sortedList[i].second) {
-                break
-            }
-            if (sortedList[j].first >= sortedList[i].first && sortedList[j].first <= sortedList[i].second) {
-                count++
-            }
-            if (count > 10_000_000) {
+    upper.sort()
+    lower.sort()
+    var j = 0
+    for (i in 0 until N) {
+        while ( j < N && upper[i] >= lower[j]) {
+            cnt += j
+            cnt -= i
+            j++
+            if (cnt > 10_000_000) {
                 return -1
             }
         }
     }
-    return count
+    return cnt
 }
