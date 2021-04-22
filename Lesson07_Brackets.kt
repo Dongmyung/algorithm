933014e9177db6157e801f6e4751e47d858b1465
Dongmyung Ahn <crazyjumong@gmail.com>
2021-04-22 10:30:54 +0900

    Create Lesson07_Brackets.kt

---
+++ Lesson07_Brackets.kt
@@ -0,0 +1,34 @@
+import java.util.*
+fun solution(S: String): Int {
+    val charArr = S.toCharArray()
+    var stack = Stack<Char>()
+    for (c in charArr) {
+        if (isStartNesting(c)) {
+            stack.push(c)
+        } else {
+            if (stack.size > 0) {
+                val cPop = stack.pop()
+                if (cPop == null || !isMatchingNestingChar(cPop, c)) return 0
+            } else {
+                return 0
+            }
+        }
+    }
+    if (stack.size > 0) return 0
+    return 1
+}
+
+private fun isStartNesting(c: Char): Boolean {
+    // Assume the char c consists only of the following characters: "(", "{", "[", "]", "}" and/or ")"
+    // Otherwise, need to check validity
+    if (c == '(' || c == '{' || c == '[') return true
+    else return false
+}
+private fun isMatchingNestingChar(c1: Char, c2: Char): Boolean {
+    // Assume the char c consists only of the following characters: "(", "{", "[", "]", "}" and/or ")"
+    // Otherwise, need to check validity
+    if (c1 == '(' && c2 == ')') return true
+    if (c1 == '{' && c2 == '}') return true
+    if (c1 == '[' && c2 == ']') return true
+    return false
+}
